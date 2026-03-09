20cf6183eded50da3ff7b2214a1feafc00ec9ec6
Jesus M. Garcia <jesusmgarciadev@gmail.com>
2026-03-09 23:08:38 +0100

    use stored authentication to navigate the app

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import Sidebar from '../components/Sidebar';
+import MessageContainer from '../components/MessageContainer';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import type { RootState } from '../redux/store';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { authUser } = useSelector((store: RootState) => store.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!authUser) {
+      navigate('/login');
+    }
+  }, []);
+
+  return (
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden glass bg-clip-padding bg-opacity-0'>
+      <Sidebar />
+      <MessageContainer />
+    </div>
+  );
 };
 
 export default HomePage;
--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import useReducer from './userSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     user: useReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export default store;
+
+export type AppDispatch = typeof store.dispatch;
